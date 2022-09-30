10147c9281e109d0ef03746436969e31e63de06f
Gabriel Frasson <bielfc27@gmail.com>
2022-09-30 16:42:46 -0300

    Generate certificate with dynamic data

--- serverless.ts
+++ serverless.ts
@@ -20,13 +20,17 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
-    iamRoleStatements: [
-      {
-        Effect: "Allow",
-        Action: ["dynamodb:*"],
-        Resource: ["*"]
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: ["dynamodb:*"],
+            Resource: ["*"]
+          }
+        ]
       }
-    ]
+    }
   },
   // import the function via paths
   functions: {
@@ -54,6 +58,7 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ["chrome-aws-lambda"],
     },
     dynamodb: {
       stages: ['dev', 'local'] ,
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,4 +1,9 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { compile } from "handlebars";
+import { join } from "path";
+import { readFileSync } from "fs";
+import dayjs from "dayjs";
+import chromium from "chrome-aws-lambda";
 
 import { document } from '../utils/dynamodbClient';
 
@@ -6,6 +11,22 @@ interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    medal: string;
+    date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = readFileSync(filePath, "utf-8");
+
+    return compile(html)(data);
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -29,6 +50,39 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id
         }
     }).promise();
+
+    const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(medalPath, "base64");
+
+    const data: ITemplate = {
+        id,
+        name,
+        grade,
+        date: dayjs().format("DD/MM/YYYY"),
+        medal
+    }
+
+    const content = await compileTemplate(data);
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        // headless: chromium.headless,
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content);
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true,
+        path: process.env.IS_OFFLINE ? "./certificate.pdf" : null
+    });
+
+    await browser.close();
 
     return {
         statusCode: 201,
