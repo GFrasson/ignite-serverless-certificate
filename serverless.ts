11d9ca1d8bd6ee59ee9eb834588604b8ab032431
Gabriel Frasson <bielfc27@gmail.com>
2022-09-30 15:18:23 -0300

    Deploy

--- serverless.ts
+++ serverless.ts
@@ -11,6 +11,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: "us-east-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -19,6 +20,13 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:*"],
+        Resource: ["*"]
+      }
+    ]
   },
   // import the function via paths
   functions: {
